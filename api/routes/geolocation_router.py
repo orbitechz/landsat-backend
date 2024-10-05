386cec7856eacab3aae06a4ff6826bfa69a75141
Jean Buss <jean.buss@itaipuparquetec.org.br>
2024-10-05 19:22:42 -0300

    Sentinel tests

--- api/routes/geolocation_router.py
+++ api/routes/geolocation_router.py
@@ -7,5 +7,18 @@ geolocationRouter = APIRouter()
 
 @geolocationRouter.get("/geolocation/true-color")
 async def true_color():
-    result = process_satellite_data(bbox=["-48.478889","-27.709595","-48.451810","-27.685656"], time_from=datetime(2024, 1, 1), time_to=datetime(2024, 1, 2), width=512, height=512)
-    return result
+  bbox = [-54.649086,-25.572176,-54.533558,-25.494263]  # Exemplo de bounding box
+  time_from = datetime(2021, 1, 1)
+  time_to = datetime(2021, 1, 31)
+  width = 512
+  height = 512
+  
+  # Processa os dados e obtém a imagem como resposta binária
+  image_data = await process_satellite_data(bbox, time_from, time_to, width, height)
+  
+  if image_data:
+      # Converte os dados binários da imagem em um stream de BytesIO
+      return StreamingResponse(BytesIO(image_data), media_type="image/png")
+  else:
+      print(image_data)
+      return Response(status_code=500, content="Erro ao processar imagem no Sentinel Hub")
--- api/services/sentinel.py
+++ api/services/sentinel.py
@@ -1,8 +1,13 @@
-import requests
+import httpx
+from fastapi import FastAPI, Response
+from starlette.responses import StreamingResponse
 from datetime import datetime
-# from api.classes.SentinelClient import SentinelClient
+import json
+from io import BytesIO
 
-def process_satellite_data(
+app = FastAPI()
+
+async def process_satellite_data(
         bbox: list, 
         time_from: datetime, 
         time_to: datetime, 
@@ -10,10 +15,7 @@ def process_satellite_data(
         height: int):
     
     sentinel_endpoint = "https://services-uswest2.sentinel-hub.com/api/v1/process"
-    # sentinel_client = SentinelClient()
-
-    # sentinel_token = sentinel_client.get_valid_token()  
-    sentinel_token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ3dE9hV1o2aFJJeUowbGlsYXctcWd4NzlUdm1hX3ZKZlNuMW1WNm5HX0tVIn0.eyJleHAiOjE3MjgxNjMwMjksImlhdCI6MTcyODE1OTQyOSwianRpIjoiMWUyMDYwYTgtZjhhZS00NDk0LWI4MTAtODI0MDJiN2EyZTQ4IiwiaXNzIjoiaHR0cHM6Ly9zZXJ2aWNlcy5zZW50aW5lbC1odWIuY29tL2F1dGgvcmVhbG1zL21haW4iLCJzdWIiOiJlMDY4MjQyZS04Y2E2LTRjNGQtOTc2Ni05Nzc4YWYzZGY3NGEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiI1MTJkZmYwNi0wNDc5LTQwOTUtYTg4OC1lY2YyOWM4MzVjMGQiLCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJjbGllbnRIb3N0IjoiMTc3LjczLjk3LjE4MiIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoic2VydmljZS1hY2NvdW50LTUxMmRmZjA2LTA0NzktNDA5NS1hODg4LWVjZjI5YzgzNWMwZCIsImNsaWVudEFkZHJlc3MiOiIxNzcuNzMuOTcuMTgyIiwiY2xpZW50X2lkIjoiNTEyZGZmMDYtMDQ3OS00MDk1LWE4ODgtZWNmMjljODM1YzBkIiwiYWNjb3VudCI6ImU1ZTA4NTg4LTZjY2QtNDU1NS1hYjczLThmMTY4Y2YzZjEzYyJ9.mnFLxbRxcDReoBAS_AiBB9im4RvYVpbaUIkhyJH5LauKrXU0gDY27VSoAWcLOr6H_r8Bw2QTwIQx06d1IRpWi63JQOVUX3eSIgioGZtXmvplQXH1BqUsGSh8jsMzyKWS9DjYZLlHRlufmarDJ09X2Bs9DJrABSAezuHa70rAk2W3B2y37MlmXMLFI5FPX-9c2EOwammMlm1owBchBSYW-Tl-VDoXrk27EJZyn0lVKS0P50R_hNID6XJZXb_P1HcWzGTZTxyHiDHgqEIVgdz7I_YUhZd37KrMYpmJl85RBcjKDVrXTRWsNz4BqACgBJll3gHSYlcRhfnlMNkc-o94uw"  
+    sentinel_token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ3dE9hV1o2aFJJeUowbGlsYXctcWd4NzlUdm1hX3ZKZlNuMW1WNm5HX0tVIn0.eyJleHAiOjE3MjgxNjY1ODMsImlhdCI6MTcyODE2Mjk4MywianRpIjoiM2ViMzc1ZjAtYmNkZi00M2UyLWEwOTQtMDc4Y2ZmMjBmNzE0IiwiaXNzIjoiaHR0cHM6Ly9zZXJ2aWNlcy5zZW50aW5lbC1odWIuY29tL2F1dGgvcmVhbG1zL21haW4iLCJzdWIiOiJlMDY4MjQyZS04Y2E2LTRjNGQtOTc2Ni05Nzc4YWYzZGY3NGEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiI1MTJkZmYwNi0wNDc5LTQwOTUtYTg4OC1lY2YyOWM4MzVjMGQiLCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJjbGllbnRIb3N0IjoiMTc3LjczLjk3LjE4MiIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoic2VydmljZS1hY2NvdW50LTUxMmRmZjA2LTA0NzktNDA5NS1hODg4LWVjZjI5YzgzNWMwZCIsImNsaWVudEFkZHJlc3MiOiIxNzcuNzMuOTcuMTgyIiwiY2xpZW50X2lkIjoiNTEyZGZmMDYtMDQ3OS00MDk1LWE4ODgtZWNmMjljODM1YzBkIiwiYWNjb3VudCI6ImU1ZTA4NTg4LTZjY2QtNDU1NS1hYjczLThmMTY4Y2YzZjEzYyJ9.aV90x-0m83CtJ2G7AXlGrkJYAUcwLxchn-bdm92qEVnHRt2p1s7QS6SIW1SYlM7sPghTsfM4RAEEAH1C-mNXWXXPSMQNpJDqjOGDP9fok9dj2ULimwt0__Wu538kWFfaYaQs4OCJ3cz3LxuF3Vk38ItuIcZRvrdXHTu1Duv_Q2IORo-2NomegFyF4s_cQTShy6a8Tx18nzfqayKmmHHcIycoqIOGET5nla_KUSgg5Qr8M628voz6NaWbN4q4G0Snne1LFpMgj9cUK5bdBx60wiFxXYvRSOiedlFm_JuFPIZhpMLOy4dIiOFttJABD4EwhTmUcDONF4evZXeX5m7fVw"
 
     headers = {
         'Authorization': f'Bearer {sentinel_token}',
@@ -64,17 +66,23 @@ def process_satellite_data(
     }
     """
     
+    request_data_json = json.dumps(request_data)
+
     files = {
-        'request': (None, str(request_data), 'application/json'),
-        'evalscript': (None, evalscript, 'text/plain'),
+        'request': (None, request_data_json, 'application/json'),  # Enviar request_data como JSON
+        'evalscript': (None, evalscript, 'text/plain'),  # Enviar evalscript como texto
     }
-    
-    response = requests.post(sentinel_endpoint, headers=headers, files=files)
-    
-    if response.status_code == 200:
-        return response.json()  
-    else:
-        return {
-            "error": response.status_code,
-            "message": response.text
-        }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            sentinel_endpoint, 
+            headers=headers, 
+            files=files
+        )
+        print(response.content)
+        if response.status_code == 200:
+            # Retorna os dados binários da imagem
+            return response.content
+        else:
+            return None
+
