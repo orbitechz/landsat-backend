c1174233ad81811ac64a559733d47b50ed4b90ce
Jean Buss <jean.buss@itaipuparquetec.org.br>
2024-10-05 17:43:59 -0300

    Sentinel tests

--- api/routes/geolocation_router.py
+++ api/routes/geolocation_router.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
+import datetime
+
+from api.services.sentinel import *
 
 geolocationRouter = APIRouter()
 
 @geolocationRouter.get("/geolocation/true-color")
 async def true_color():
-    return {"message": "Hello World"}
+    result = process_satellite_data(bbox=["-48.478889","-27.709595","-48.451810","-27.685656"], time_from=datetime(2024, 1, 1), time_to=datetime(2024, 1, 2), width=512, height=512)
+    return result
--- api/services/sentinel.py
+++ api/services/sentinel.py
@@ -1,51 +1,51 @@
 import requests
 from datetime import datetime
+# from api.classes.SentinelClient import SentinelClient
 
 def process_satellite_data(
-        access_token: str, 
         bbox: list, 
         time_from: datetime, 
         time_to: datetime, 
         width: int, 
         height: int):
     
-    url = "https://services-uswest2.sentinel-hub.com/api/v1/process"
-    
-    # Cabeçalhos da requisição
+    sentinel_endpoint = "https://services-uswest2.sentinel-hub.com/api/v1/process"
+    # sentinel_client = SentinelClient()
+
+    # sentinel_token = sentinel_client.get_valid_token()  
+    sentinel_token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ3dE9hV1o2aFJJeUowbGlsYXctcWd4NzlUdm1hX3ZKZlNuMW1WNm5HX0tVIn0.eyJleHAiOjE3MjgxNjMwMjksImlhdCI6MTcyODE1OTQyOSwianRpIjoiMWUyMDYwYTgtZjhhZS00NDk0LWI4MTAtODI0MDJiN2EyZTQ4IiwiaXNzIjoiaHR0cHM6Ly9zZXJ2aWNlcy5zZW50aW5lbC1odWIuY29tL2F1dGgvcmVhbG1zL21haW4iLCJzdWIiOiJlMDY4MjQyZS04Y2E2LTRjNGQtOTc2Ni05Nzc4YWYzZGY3NGEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiI1MTJkZmYwNi0wNDc5LTQwOTUtYTg4OC1lY2YyOWM4MzVjMGQiLCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJjbGllbnRIb3N0IjoiMTc3LjczLjk3LjE4MiIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoic2VydmljZS1hY2NvdW50LTUxMmRmZjA2LTA0NzktNDA5NS1hODg4LWVjZjI5YzgzNWMwZCIsImNsaWVudEFkZHJlc3MiOiIxNzcuNzMuOTcuMTgyIiwiY2xpZW50X2lkIjoiNTEyZGZmMDYtMDQ3OS00MDk1LWE4ODgtZWNmMjljODM1YzBkIiwiYWNjb3VudCI6ImU1ZTA4NTg4LTZjY2QtNDU1NS1hYjczLThmMTY4Y2YzZjEzYyJ9.mnFLxbRxcDReoBAS_AiBB9im4RvYVpbaUIkhyJH5LauKrXU0gDY27VSoAWcLOr6H_r8Bw2QTwIQx06d1IRpWi63JQOVUX3eSIgioGZtXmvplQXH1BqUsGSh8jsMzyKWS9DjYZLlHRlufmarDJ09X2Bs9DJrABSAezuHa70rAk2W3B2y37MlmXMLFI5FPX-9c2EOwammMlm1owBchBSYW-Tl-VDoXrk27EJZyn0lVKS0P50R_hNID6XJZXb_P1HcWzGTZTxyHiDHgqEIVgdz7I_YUhZd37KrMYpmJl85RBcjKDVrXTRWsNz4BqACgBJll3gHSYlcRhfnlMNkc-o94uw"  
+
     headers = {
-        'Authorization': f'Bearer {access_token}',
+        'Authorization': f'Bearer {sentinel_token}',
     }
     
-    # Convertendo as datas para o formato ISO 8601 (UTC)
     time_from_str = time_from.strftime("%Y-%m-%dT%H:%M:%SZ")
     time_to_str = time_to.strftime("%Y-%m-%dT%H:%M:%SZ")
     
-    # Dados da requisição com parâmetros customizáveis
     request_data = {
         "input": {
             "bounds": {
                 "properties": {
                     "crs": "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
                 },
-                "bbox": bbox  # Customizado via argumento
+                "bbox": bbox  
             },
             "data": [{
                 "type": "landsat-ot-l2",
                 "dataFilter": {
                     "timeRange": {
-                        "from": time_from_str,  # Customizado via argumento
-                        "to": time_to_str      # Customizado via argumento
+                        "from": time_from_str, 
+                        "to": time_to_str     
                     }
                 }
             }]
         },
         "output": {
-            "width": width,  # Customizado via argumento
-            "height": height # Customizado via argumento
+            "width": width, 
+            "height": height 
         }
     }
     
-    # Script de avaliação (fixo, pode ser ajustado se necessário)
     evalscript = """
     //VERSION=3
 
@@ -64,18 +64,15 @@ def process_satellite_data(
     }
     """
     
-    # Enviar dados como multipart/form-data
     files = {
         'request': (None, str(request_data), 'application/json'),
         'evalscript': (None, evalscript, 'text/plain'),
     }
     
-    # Realiza a requisição POST
-    response = requests.post(url, headers=headers, files=files)
+    response = requests.post(sentinel_endpoint, headers=headers, files=files)
     
-    # Verifica se a resposta foi bem sucedida
     if response.status_code == 200:
-        return response.json()  # Retorna os dados como JSON
+        return response.json()  
     else:
         return {
             "error": response.status_code,
