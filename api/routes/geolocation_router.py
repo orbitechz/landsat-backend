902b8d6c2042461c30e1bc589d8c28fee12fd6d4
Jean Buss <jean.buss@itaipuparquetec.org.br>
2024-10-05 15:33:12 -0300

    sentinel.py

--- api/routes/geolocation_router.py
+++ api/routes/geolocation_router.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 geolocationRouter = APIRouter()
 
 @geolocationRouter.get("/geolocation/true-color")
-async def teste():
+async def true_color():
     return {"message": "Hello World"}
