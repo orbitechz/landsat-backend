4c6c9fc5942cad3e95ffc3ee0fc25f10eac1052c
Jean Buss <jean.buss@itaipuparquetec.org.br>
2024-10-06 11:35:46 -0300

    Sentinel tests

--- api/routes/earthengine_router.py
+++ api/routes/earthengine_router.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 import ee
-import geemap 
+from starlette.responses import JSONResponse
+
+from api.classes.SentinelClient import SentinelClient
+
 
 # Inicializando a
 # API do Earth Engine
 ee.Authenticate()
 ee.Initialize(project='landsat-437722')
-
 
 
 earthengineRouter = APIRouter()
@@ -27,7 +29,7 @@ async def get_landsat_data(request: Request):
     region = point.buffer(500)
 
     # Coleção Landsat 8 Surface Reflectance
-    landsat_collection = ee.ImageCollection("LANDSAT/LC08/C02/T1_RT_TOA") \
+    landsat_collection = ee.ImageCollection("") \
         .filterBounds(region) \
         .filterDate(start_date, end_date) \
         .limit(10)  # Limitando para 5 imagens
@@ -52,3 +54,45 @@ async def get_landsat_data(request: Request):
 
     # Retornando os URLs das imagens
     return JSONResponse(content=urls)
+
+@earthengineRouter.get("/gee-data2")
+async def get_gee_data():
+    # Exemplo de filtro Landsat
+    collection = ee.ImageCollection('LANDSAT/LC09/C02/T1_TOA')\
+        .filterDate('2012-01-01', '2024-12-31')\
+        .filterBounds(ee.Geometry.Point([-54.570679921692374, -25.55270154839337 ]))
+
+    # Pegando as informações de metadados como exemplo
+    info = collection.getInfo()
+    return info
+  
+@earthengineRouter.get("/gee-data")
+async def get_gee_urls():
+    # Coleção de imagens do Landsat
+    collection = ee.ImageCollection('LANDSAT/LC09/C02/T2_L2')
+
+    # Definir a área de interesse (opcional)
+    region = ee.Geometry.Polygon(
+        [[[-53.835775936665506, -25.41450954035195],
+          [-53.82182654195521, -25.35547641863467],
+          [-56.076285147173806, -26.668919417447714],
+          [-55.681717409573864, -26.75053519952937]]]
+    )
+
+    # Selecionar a imagem mais recente e visualizar a banda 4 (Red), 3 (Green) e 2 (Blue)
+    image = collection.filterBounds(region).first()
+
+    # Definir parâmetros de visualização (usando bandas 4, 3 e 2 do Landsat 9)
+    vis_params = {
+        'bands': ['SR_B4', 'SR_B3', 'SR_B2'],
+        'min': 0,
+        'max': 3000,
+        'gamma': 1.4
+    }
+
+    # Gerar URL de tiles para o Leaflet
+    map_id_dict = ee.Image(image).getMapId(vis_params)
+    tile_url = map_id_dict['tile_fetcher'].url_format
+
+    # Retornar a URL de tiles para o frontend
+    return JSONResponse({"tile_url": tile_url})
--- api/services/sentinel.py
+++ api/services/sentinel.py
@@ -12,10 +12,11 @@ async def process_satellite_data(
         time_from: datetime, 
         time_to: datetime, 
         width: int, 
-        height: int):
+        height: int,
+        sentinel_token: str):
     
     sentinel_endpoint = "https://services-uswest2.sentinel-hub.com/api/v1/process"
-    sentinel_token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ3dE9hV1o2aFJJeUowbGlsYXctcWd4NzlUdm1hX3ZKZlNuMW1WNm5HX0tVIn0.eyJleHAiOjE3MjgxNjY1ODMsImlhdCI6MTcyODE2Mjk4MywianRpIjoiM2ViMzc1ZjAtYmNkZi00M2UyLWEwOTQtMDc4Y2ZmMjBmNzE0IiwiaXNzIjoiaHR0cHM6Ly9zZXJ2aWNlcy5zZW50aW5lbC1odWIuY29tL2F1dGgvcmVhbG1zL21haW4iLCJzdWIiOiJlMDY4MjQyZS04Y2E2LTRjNGQtOTc2Ni05Nzc4YWYzZGY3NGEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiI1MTJkZmYwNi0wNDc5LTQwOTUtYTg4OC1lY2YyOWM4MzVjMGQiLCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJjbGllbnRIb3N0IjoiMTc3LjczLjk3LjE4MiIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoic2VydmljZS1hY2NvdW50LTUxMmRmZjA2LTA0NzktNDA5NS1hODg4LWVjZjI5YzgzNWMwZCIsImNsaWVudEFkZHJlc3MiOiIxNzcuNzMuOTcuMTgyIiwiY2xpZW50X2lkIjoiNTEyZGZmMDYtMDQ3OS00MDk1LWE4ODgtZWNmMjljODM1YzBkIiwiYWNjb3VudCI6ImU1ZTA4NTg4LTZjY2QtNDU1NS1hYjczLThmMTY4Y2YzZjEzYyJ9.aV90x-0m83CtJ2G7AXlGrkJYAUcwLxchn-bdm92qEVnHRt2p1s7QS6SIW1SYlM7sPghTsfM4RAEEAH1C-mNXWXXPSMQNpJDqjOGDP9fok9dj2ULimwt0__Wu538kWFfaYaQs4OCJ3cz3LxuF3Vk38ItuIcZRvrdXHTu1Duv_Q2IORo-2NomegFyF4s_cQTShy6a8Tx18nzfqayKmmHHcIycoqIOGET5nla_KUSgg5Qr8M628voz6NaWbN4q4G0Snne1LFpMgj9cUK5bdBx60wiFxXYvRSOiedlFm_JuFPIZhpMLOy4dIiOFttJABD4EwhTmUcDONF4evZXeX5m7fVw"
+
 
     headers = {
         'Authorization': f'Bearer {sentinel_token}',
@@ -44,26 +45,35 @@ async def process_satellite_data(
         },
         "output": {
             "width": width, 
-            "height": height 
+            "height": height, 
+            "responses": [{
+              "identifier": "default",
+              "format": {
+                "type": "image/tiff"
+              }
+            }]
         }
     }
-    
+      
     evalscript = """
-    //VERSION=3
-
-    function setup() {
-        return {
-            input: ["B02", "B03", "B04"],
-            output: {
-                bands: 3,
-                sampleType: "AUTO" // default value - scales the output values from [0,1] to [0,255].
-            }
-        };
+  //VERSION=3
+function setup() {
+    return {
+      input: [{
+        bands: [
+          "B10"
+        ]
+      }],
+      output: {
+        bands: 1,
+        sampleType: "FLOAT32"
+      }
     }
+  }
 
-    function evaluatePixel(sample) {
-        return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
-    }
+  function evaluatePixel(samples) {
+    return [samples.B10]
+  }
     """
     
     request_data_json = json.dumps(request_data)
